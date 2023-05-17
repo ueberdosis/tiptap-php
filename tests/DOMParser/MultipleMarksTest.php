0d0b83b580bf33b7b036b51e1a727ffb453b7622
Joachim <kueng@falter.at>
2023-05-17 17:47:38 +0200

    revert mistaken change

--- tests/DOMParser/MultipleMarksTest.php
+++ tests/DOMParser/MultipleMarksTest.php
@@ -4,6 +4,7 @@ use Tiptap\Editor;
 
 test('multiple marks are rendered correctly', function () {
     $html = '<p><strong><em>Example Text</em></strong></p>';
+
     $result = (new Editor)->setContent($html)->getDocument();
 
     expect($result)->toEqual([

d30a8c574f3b7e919b001af690148c41ce366228
Timo Isik <mail@timoisik.de>
2025-02-12 17:51:05 +0100

    Fix whitespace in expected HTML output for CodeBlockShikiTest

--- tests/DOMSerializer/Nodes/CodeBlockShikiTest.php
+++ tests/DOMSerializer/Nodes/CodeBlockShikiTest.php
@@ -116,5 +116,5 @@ test('html result is properly rendered', function () {
         ->getHtml();
 
     expect($result)
-        ->toEqual('<p><code>Example Text</code></p><pre class="shiki" style="background-color: #2e3440ff"><code><span class="line"><span style="color: #81A1C1">body</span><span style="color: #D8DEE9FF"> </span><span style="color: #ECEFF4">{</span><span style="color: #D8DEE9FF"> </span><span style="color: #D8DEE9">display</span><span style="color: #ECEFF4">:</span><span style="color: #D8DEE9FF"> </span><span style="color: #81A1C1">none</span><span style="color: #D8DEE9FF"> </span><span style="color: #ECEFF4">}</span></span></code></pre>');
+        ->toEqual('<p><code>Example Text</code></p><pre class="shiki" style="background-color: #2e3440ff"><code><span class="line"><span style="color:#81A1C1">body</span><span style="color:#D8DEE9FF"> </span><span style="color:#ECEFF4">{</span><span style="color:#D8DEE9FF"> </span><span style="color:#D8DEE9">display</span><span style="color:#ECEFF4">:</span><span style="color:#D8DEE9FF"> </span><span style="color:#81A1C1">none</span><span style="color:#D8DEE9FF"> </span><span style="color:#ECEFF4">}</span></span></code></pre>');
 });
